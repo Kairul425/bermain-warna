70dd67d8a6840612a23f42bb85e656d605ce14e3
kairul04 <kairulsky4@gmail.com>
2023-08-17 06:44:55 +0700

    menambah fitur mouse hover

--- script.js
+++ script.js
@@ -76,3 +76,12 @@ sHijau.addEventListener('input', function () {
 sBiru.addEventListener('input', function () {
   tBiru.innerHTML = sBiru.value;
 });
+
+// kursor mouse
+
+document.body.addEventListener('mousemove', function (event) {
+  const xPos = Math.round((event.clientX / window.innerWidth) * 255);
+  const yPos = Math.round((event.clientY / window.innerHeight) * 255);
+
+  document.body.style.backgroundColor = 'rgb(' + xPos + ',' + yPos + ',100)';
+});
